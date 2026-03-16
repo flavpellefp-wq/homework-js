a9d7a73cd414ae4c689115dbac1d4363bcfd72d8
flavpellefp-wq <flavpelle.fp@gmail.com>
2026-03-16 19:01:01 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function cambiaTesto() {
+ document.getElementById("mioDiv").innerHTML =
+ "Il testo è stato modificato con JavaScript!";
+}
+
+function cambiaColore() {
+ document.getElementById("mioDiv").style.color = "red";
+}
+
+function aggiungiTesto() {
+ document.getElementById("mioDiv").innerHTML +=
+ "<br>Nuovo contenuto aggiunto!";
+}
